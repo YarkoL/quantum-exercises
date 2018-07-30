d694edcc570ee1c319db64cd3a0d09f48d0bd061
YarkoL <yarko.dev@gmail.com>
2018-07-30 19:46:04 +0300

    added test for unitarity

--- hermitian_test.py
+++ hermitian_test.py
@@ -2,23 +2,32 @@
 
 import matrix_arithmetic as mat
 
-testA = [[(7,0),(6,5)],[(6,-5),(2,0)]]
+testH = [[(7,0),(6,5)],[(6,-5),(2,0)]]
+testU = [[(1,0),(0,0)],[(0,0),(0,1)]]
+testM = [[(5,0),(2,-1)],[(0,0),(4,5)]]
 testing = False
 
 def start():
   if testing:
-  	A = testA
+  	A = testM
   else:	
-    print "Test if a complex nxn-matrix is hermitian"
+    print "Test if a complex nxn-matrix is hermitian or unitary"
     n = input("Enter dimension n  :  ")
     A = mat.init_matrix(n,n)
     A = mat.populate_matrix(A)
   print("Is this matrix hermitian : ", is_hermitian(A))
+  print("Is this matrix unitary : ", is_unitary(A))
 
 def is_hermitian(A):
   A_transposed = mat.transpose(A)
   A_conjugated = mat.conjugate(A)
-  return A_transposed == A_conjugated 	   
+  return A_transposed == A_conjugated 	  
+
+def is_unitary(A):
+  I = mat.get_identity_matrix(len(A))
+  A_dagger = mat.conjugate(mat.transpose(A))   
+  P = mat.matrix_multiply(A_dagger, A)
+  return mat.equals(P,I)
 
 if __name__ == '__main__':
   start()
