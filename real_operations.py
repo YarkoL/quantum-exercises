426bbf5f4d51dd4a2cffbfeeba95da3fa065de0d
YarkoL <yarko.dev@gmail.com>
2018-08-02 22:59:59 +0300

    real vectors, matrices

---
+++ real_operations.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python2
+
+def start():
+  A = [[1,2,3]]
+  B = [[2,1,3],[3,3,2],[4,1,2]]
+  print matrix_multiply(A,B)
+
+def create_vector(n):
+  v = []	
+  for i in range(n):
+    r = input("Enter real at place " + str(i) + "  ")
+    v.append(r)
+  return v 
+  
+def init_matrix(m,n):
+  M = [0] * m
+  for i in range(m):
+    M[i] = [0] * n
+  return M	
+
+def populate_matrix(M):
+  rows = len(M)
+  cols = len(M[0]) 	
+  for i in range(rows):
+    for j in range(cols):
+      r = input("Enter real number at " + str(i) + "," + str(j) + " : ")
+      M[i][j] = r
+  return M
+
+def add(A,B):
+  rows = len(A)
+  cols = len(A[0])
+  res = init_matrix(rows,cols)
+  for j in range(rows):
+    for k in range(cols):
+      res[j][k] = A[j][k]+B[j][k]
+  return res
+
+def substract(A,B):
+  rows = len(A)
+  cols = len(A[0])
+  res = init_matrix(rows,cols)
+  for j in range(rows):
+    for k in range(cols):
+      res[j][k] = A[j][k]-B[j][k]
+  return res    
+
+def matrix_multiply(A,B):
+  rowsA = len(A)
+  colsB = len(B[0])
+  res = init_matrix(rowsA,colsB)
+  for j in range(rowsA):
+    for k in range(colsB):
+      el = 0
+      for i in range(len(B)):
+        p = A[j][i] * B[i][k]
+        el += p
+      res[j][k] = el         
+  return res  
+
+if __name__ == '__main__':
+  start()
+
