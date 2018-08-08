a55ab74ecbda45854f35c99b15a2623da915ac0b
YarkoL <yarko.dev@gmail.com>
2018-08-08 21:10:21 +0300

    arbitary length + small fixes

--- vectors_inner_product.py
+++ vectors_inner_product.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python2
-
+from __future__ import division
 import matrix_arithmetic as mat
 import complex_arithmetic as ar
 import math
+testing = True
 
-n = 3
 def start():
-  print("Let's calculate things based on inner product of two complex " + str(n) +"-vectors.")
-  V1 = mat.init_matrix(1,n)
-  V2 = mat.init_matrix(1,n)
-  print ("Enter first vector")
-  V1 = mat.populate_matrix(V1)
-  print(V1)
-  print("Length : " + str(length(V1)))
-  print ("Enter second vector")
-  V2 = mat.populate_matrix(V2)
+  if testing:
+  	V1 = [[(0.0, 0.707), (-0.707, 0.0)]]
+  	V2 = [[(0.707, 0.0), (0.0, 0.707)]]
+  else:  	
+    print("Let's calculate things based on inner product of two complex vectors.")
+    n = input("Length of the vectors ")
+    V1 = mat.init_matrix(1,n)
+    V2 = mat.init_matrix(1,n)
+    print ("Enter first vector")
+    V1 = mat.populate_matrix(V1)
+   
+    print ("Enter second vector")
+    V2 = mat.populate_matrix(V2)
+    
+  print(V1)  
+  print("Length  : " + str(length(V1)))  
   print(V2)
   print("Length : " + str(length(V2)))
   inner = inner_product(V1,V2)
@@ -27,8 +34,7 @@ def start():
    
 def inner_product(A,B):
   res = (0,0)
-  #A_transpose = mat.transpose(A)	
-  #print(A_transpose)
+  n = len(A[0])
   A_ = mat.conjugate(A)	
   for i in range(n):
   	p = ar.multiply(A_[0][i],B[0][i])
@@ -37,7 +43,7 @@ def inner_product(A,B):
   
 def length(A):
   (re,im) = inner_product(A,A)
-  return math.sqrt(re)
+  return round(math.sqrt(re),3)
 
 def distance(A,B):
   diff = mat.substract(A,B)
