fa54bb40b030ae569b157e96d3857971ed5afb22
YarkoL <yarko.dev@gmail.com>
2018-08-13 15:05:06 +0300

    expected value

---
+++ observable.py
@@ -0,0 +1,32 @@
+from __future__ import division
+import math
+import matrix_arithmetic as mat
+import hermitian_test as check
+import vectors_inner_product as inner
+
+def start():
+  print("Let's calculate some values related to an observable in a quantum system")
+  n = input("Enter the dimension of the hermitian matrix of the observable   : ")
+  H = mat.init_matrix(n,n) 	
+  mat.populate_matrix(H)
+  if check.is_hermitian(H) == False:
+  	print "Not a hermitian matrix! Bye."
+  	return
+  print(H)	
+  print("Input a ket vector")
+  psi = mat.init_matrix(1,n)
+  mat.populate_matrix(psi)
+  a,b = expected_value(H,psi)
+  if b != 0:
+    print "Expected value ought to be a real number, but something went wrong"
+    print a,b
+    return
+  print("--------------------------------------------------")
+  print("Expected value of observable on ket : " + str(int(a)))
+
+def expected_value(H,psi):
+  h_psi = mat.matrix_multiply(H,psi)	
+  return inner.inner_product(h_psi,psi)
+
+if __name__ == '__main__':
+  start()
